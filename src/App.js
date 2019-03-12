7246a52fa2fefd91438422e79304d9b4bf55d8a4
Joe Widener <jwidener08@gmail.com>
2019-03-12 18:46:20 -0400

    added basic functionality

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.scss';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faAward, faFlask, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faAward, faFlask, faTrash, faPlus } from '@fortawesome/free-solid-svg-icons';
+
 
 import Navigation from './components/Navigation';
 import TodoList from './components/TodoList';
@@ -17,30 +18,79 @@ const LINKS = [
   { label: 'LinkedIn', to: 'https://www.linkedin.com', icon: 'award'}
 ];
 
-const TODO_ITEMS = [
-    {id: 1, item: 'Ommpa Loompa'},
-    {id: 2, item: 'Huzzah'},
-    {id: 3, item: 'wheeeeee!!' }
-];
 
-library.add(faHome, faAward, faFlask);
+
+library.add(faHome, faAward, faFlask, faTrash, faPlus);
 
 
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: []
+        };
+
+        // These deal with "this" being undefined
+        this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+    }
+
+  addItem(event) {
+        if(this._inputElement.value !== ''){
+            let newItem = {
+                text: this._inputElement.value,
+                key: Date.now()
+            };
+            this.setState((prevState) => {
+                return {
+                    items: prevState.items.concat(newItem)
+                };
+
+            });
+        }
+
+        this._inputElement.value = '';
+        console.log(this.state.items);
+
+      event.preventDefault();
+  }
+
+  deleteItem(key)  {
+       let filteredItems = this.state.items.filter((items) => {
+           return (items.key !== key)
+       });
+
+       this.setState({
+           items: filteredItems
+       });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <div className="logo-container">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1>React To Do List</h1>
+            <h1>To Do List</h1>
           </div>
 
           <Navigation links={LINKS}/>
         </header>
         <section className="App-content">
-            <TodoList items={TODO_ITEMS}/>
+            <div className="input-section">
+                <form className="App" onSubmit={this.addItem}>
+                    <input ref={(a) => this._inputElement = a} placeholder="Enter a task"/>
+                    <button title="Add">
+                        <FontAwesomeIcon icon="plus"/>
+                    </button>
+                </form>
+            </div>
+            <div className="list-section">
+                <TodoList  items={this.state.items}
+                            delete={this.deleteItem} />
+            </div>
         </section>
        <footer className="App-footer">
          {/*Put footer stuff here*/}
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import FlipMove from 'react-flip-move';
 
 import './todolist.scss';
 
-const TodoList = ({ items }) => (
-    <div className="TodoList">
-        <ul>{ items.map( items => (
-                <li id={items.id} key={items.id}>
-                    {items.item}
-                    <FontAwesomeIcon icon="trash"/>
-                </li>
-                ))
-            }
-        </ul>
-    </div>
-)
+class TodoList extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.createTasks = this.createTasks.bind(this);
+    }
+
+    deleteTasks(key) {
+        this.props.delete(key);
+    };
+
+    createTasks(items) {
+        return <li key={items.key}>{items.text} <span className="delete-button" onClick={() => this.deleteTasks(items.key)}><FontAwesomeIcon icon="trash"/></span></li>
+    };
+
+    render() {
+        let todoItems = this.props.items;
+        let listItems = todoItems.map(this.createTasks);
+
+        return(
+            <div className="TodoList">
+                <ul>
+                    <FlipMove duration={250} easing="ease-out" enterAnimation="accordionVertical">
+                        {listItems}
+                    </FlipMove>
+                </ul>
+            </div>
+        )
+    }
+}
+
 
 export default TodoList;
